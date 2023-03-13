0c3ffd9be936861d8774ca4d793e465dc363f0b1
Emil Zyka <emil.zyka@consid.se>
2023-03-13 11:46:26 +0100

    updated posts, added utils and article layout temp.

---
+++ src/utils/functions.js
@@ -0,0 +1,24 @@
+//Add more parameters to make add more filters.
+export function filterArticlePost(posts, { sortByDate = true, limit = undefined } = {}) {
+    /*
+    const filteredPosts = posts.reduce((acc, post) => {
+        acc.push(post);
+        return acc;
+    }, []);
+
+    */
+
+    //sort by date or randomize
+    if (sortByDate) {
+        posts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+    } else {
+        posts.sort(() => Math.random() - 0.5);
+    }
+
+    //limit
+    if (typeof limit === 'number') {
+        return posts.slice(0, limit);
+    }
+
+    return posts;
+}
