1e43e5295828309162e65defb27d80f49637934b
emil-zyka <95310820+emilzyka@users.noreply.github.com>
2023-03-13 14:55:03 +0100

    styled articles page

--- src/components/PostCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const PostCard = ({ postData }) => {
-    return (
-        <div>
-            <a href={postData.frontmatter.path}>{postData.frontmatter.title}</a>
-        </div>
-    );
-};
