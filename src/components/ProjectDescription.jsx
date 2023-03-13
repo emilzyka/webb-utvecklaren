6b3eb44192b5ccc3dc82611ef20f3af34f8d9b99
emil-zyka <95310820+emilzyka@users.noreply.github.com>
2023-03-13 16:32:33 +0100

    changed project description

--- src/components/ProjectDescription.jsx
+++ src/components/ProjectDescription.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const ProjectDescription = () => {
   return (
     <section className='introduction' aria-label='introduction'>
-      <div className='flex flex-col items-center justify-center'>
+      <div className='flex flex-col items-center justify-center mt-8'>
         <div className='flex flex-col w-2/5 sm:w-4/5 text-justify'>
           <h1
             className='text-left text-3xl font-normal text-transparent 
@@ -11,7 +11,7 @@ export const ProjectDescription = () => {
                      decoration-purple-200 decoration-1 underline-offset-2 mb-2'>
             Introduktion.
           </h1>
-          <p className='text-black font-light dark:text-white'>
+          <p className='text-black font-light dark:text-white text-lg'>
             Välkommen till Webbutvecklaren. En samlingsplats där du kan lära dig
             om grundläggande ämnen i modern webbutveckling. Hemsidan samlar
             resurser vars ändamål är att lära eller bistå dig i ditt lärande av
@@ -27,16 +27,14 @@ export const ProjectDescription = () => {
             className='text-left text-3xl font-normal text-transparent 
                     bg-gradient-to-r from-purple-200 to-purple-900 bg-clip-text underline
                      decoration-purple-200 decoration-1 underline-offset-2 mb-2'>
-            Om projektet.
+            Om Sidan.
           </h1>
-          <p className='text-black font-light dark:text-white'>
+          <p className='text-black font-light dark:text-white text-lg'>
             Sidan riktar sig framför allt till dig som är helt ny inom
-            webbutvecklng och vill lära dig grunderna. Projektet är fortfarande
-            under utveckling och du kan förvänta dig helt nya tillägg, artiklar
-            och funktionliteter på hemsidan. Feedback är alltid uppsaktat så
-            tveka inte på att höra av dig. Sidan är opinionerad, pratisk och
-            subjektiv. Innehållet som presenteras eftersträvar objektivitet men
-            tillhör författnar och bör därmed aldrig ses som fakta.
+            webbutvecklng och vill lära dig grunderna. Ambitionen är att samla
+            värdefulla resurser, artiklar och även kodexempel så att du kan
+            påbörja din resa som webbutvecklare här. Den här webbplatesen och
+            dess innehåll är framtaget av Emil Zyka.
           </p>
         </div>
       </div>
